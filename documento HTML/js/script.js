1910d2cc1a628e9a84b5c9158d54ee288d10acac
Thiago <thiagoalgusto_2025@hotmail.com.br>
2022-10-06 16:24:24 -0300

    resposta numero + plus

--- documento HTML/js/script.js
+++ documento HTML/js/script.js
@@ -1,3 +1,7 @@
 
+const titulo = 'Titulo deste tuto'
 
-const cabecalho = document.getElementById('cabeca'); // para pegar o id do HTML
+const cabecalho = document.getElementById('cabeca'); // para pegar o id do HTML e trata ele no JavaScript
+
+
+cabecalho.innerHTML = titulo; // aqui eu estou fazendo o hmtl mudar via JS
--- trabalhando com numeros/js/resposta.js
+++ trabalhando com numeros/js/resposta.js
@@ -1,2 +1,16 @@
-const numero = prompt('Digite um numero seja inteiro ou não');
+const numero = Number(prompt('Digite um numero seja inteiro ou não')); // converter para numero por que o prompt 
 
+const nomeTitulo = document.getElementById('numero-titulo');
+
+const texto = document.getElementById('texto');
+
+nomeTitulo.innerHTML = `O seu número é <strong>${numero}</strong>`;
+
+texto.innerHTML = `
+    <p>A raiz quadrada é <strong>${Math.sqrt(numero)}</strong></p>
+    <p>${numero} é inteiro : <strong>${Number.isInteger(numero)}</strong>
+    <p>${numero} é NaN : <strong>${Number.isNaN(numero)}</strong>
+    <p>Arredondado para baixo fica <strong>${Math.floor(numero)}</strong></p>
+    <p>Arredondado para cima fica <strong>${Math.ceil(numero)}</strong></p>
+    <p>Com duas casas decimais fica <strong>${numero.toFixed(2)}</strong>
+    `;
